ed618b2ba8487c322f1848f1defb9b084a75163b
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2025-06-16 10:06:14 +0200

    fix: make properly compile by having adventure transitive

--- multiver/bukkit/paper/build.gradle.kts
+++ multiver/bukkit/paper/build.gradle.kts
@@ -7,7 +7,5 @@ dependencies {
     implementation(projects.skinsrestorerShared)
     implementation(projects.multiver.bukkit.shared)
 
-    compileOnly("io.papermc.paper:paper-api:1.21.5-R0.1-SNAPSHOT") {
-        isTransitive = false
-    }
+    compileOnly("io.papermc.paper:paper-api:1.21.5-R0.1-SNAPSHOT")
 }
