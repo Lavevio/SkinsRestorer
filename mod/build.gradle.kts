f2fbe96319b487bcf883ed7c9fd1147acd2c3b95
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2025-07-01 09:24:52 +0200

    fix: bump fabric api version

--- mod/build.gradle.kts
+++ mod/build.gradle.kts
@@ -119,13 +119,15 @@ dependencies {
             exclude("com.google.errorprone")
         }
     }
-
     // Shared mods
     modImplementation("net.kyori:adventure-platform-mod-shared-fabric-repack:6.5.0-SNAPSHOT")
     modImplementation("dev.architectury:architectury:17.0.6")
 
     // Mixins
     compileOnly("org.spongepowered:mixin:0.8.7")
+
+    // Bump fabric api
+    fabricModImplementation(platform("net.fabricmc.fabric-api:fabric-api-bom:0.128.1+1.21.7"))
 
     // Needed for modImplementations to load
     fabricModImplementation("net.kyori:adventure-platform-fabric:6.5.0-SNAPSHOT")
